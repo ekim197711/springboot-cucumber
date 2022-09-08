897a3c4814f7365b1d3903ddff063007f3f256d7
Mike Nielsen <mmni@protonmail.com>
2022-09-08 22:24:31 +0200

    Cucumber

--- src/test/java/com/example/springbootcucumber/cucumberglue/CucumberMySteps.java
+++ src/test/java/com/example/springbootcucumber/cucumberglue/CucumberMySteps.java
@@ -3,14 +3,13 @@ package com.example.springbootcucumber.cucumberglue;
 import io.cucumber.java.en.Then;
 import io.cucumber.java.en.When;
 import org.junit.jupiter.api.Assertions;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.test.web.server.LocalServerPort;
 import org.springframework.http.HttpMethod;
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.client.HttpClientErrorException;
 import org.springframework.web.client.RestTemplate;
-import static org.hamcrest.MatcherAssert.assertThat;
 
+import static org.hamcrest.MatcherAssert.assertThat;
 
 
 public class CucumberMySteps {
@@ -20,19 +19,31 @@ public class CucumberMySteps {
     ResponseEntity<String> lastResponse;
 
     @When("the client calls endpoint {string}")
-    public void whenClientCalls(String url){
-        lastResponse = new RestTemplate().exchange("http://localhost:" + port + url, HttpMethod.GET,null,
-                String.class);
+    public void whenClientCalls(String url) {
+        try {
+            lastResponse = new RestTemplate().exchange("http://localhost:" + port + url, HttpMethod.GET, null,
+                    String.class);
+        } catch (HttpClientErrorException httpClientErrorException) {
+            httpClientErrorException.printStackTrace();
+        }
     }
 
     @Then("response status code is {int}")
-    public void thenStatusCodee(int expected){
+    public void thenStatusCodee(int expected) {
+        Assertions.assertNotNull(lastResponse);
+        Assertions.assertNotNull(lastResponse.getStatusCode());
         assertThat("status code is" + expected,
                 lastResponse.getStatusCodeValue() == expected);
     }
+
+    @Then("response status code is not present")
+    public void thenStatusCodeeIsNotPresent() {
+        Assertions.assertNull(lastResponse);
+    }
+
     @Then("returned string should be {string}")
-    public void thenStringIs(String expected){
-        Assertions.assertEquals(expected,lastResponse.getBody());
+    public void thenStringIs(String expected) {
+        Assertions.assertEquals(expected, lastResponse.getBody());
 //        assertThat("Returned string is " + expected,
 //                expected.equalsIgnoreCase(lastResponse.getBody()));
     }
--- src/test/resources/features/CountDown.feature
+++ src/test/resources/features/CountDown.feature
@@ -1,5 +1,15 @@
-Feature: Countdown to launch
+Feature: Countdown to launch should work
+
   Scenario: Call backend with last countdown number
     When the client calls endpoint "/launch/counter/4"
     Then response status code is 200
-    And returned string should be "T-3"
+    And returned string should be "T-3xzcsdfv"
+
+  Scenario: Call backend with ONE
+    When the client calls endpoint "/launch/counter/1"
+    Then response status code is 200
+    And returned string should be "Launch! Go go go!"
+
+  Scenario: Call backend with rubbish non existing endpoint
+    When the client calls endpoint "/launch/yesyes/mycounter/1"
+    Then response status code is not present
